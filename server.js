39809d6d85d35819a725cbaf1da693742242965e
Anton Kulchytskyi <antek1809@gmail.com>
2024-03-05 23:21:18 +0200

    cors added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./db');
 const pharmacyRoutes = require('./routes/pharmacies');
 const medicineRoutes = require('./routes/medicines');
@@ -9,12 +10,13 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
+app.use(cors());
 
 // Додайте маршрути аптек та ліків
 app.use('/pharmacies', pharmacyRoutes);
 app.use('/medicines', medicineRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on port ${PORT}`);
 });
 
